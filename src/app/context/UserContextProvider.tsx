c5c8988c9b074df4fdf6c92fe764a8448ba6941d
Anmol Garg <anmolgarg271005@gmail.com>
2024-05-31 12:48:59 +0530

    "Solve minor issue"

--- src/app/context/UserContextProvider.tsx
+++ src/app/context/UserContextProvider.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react";
+import { useState, ReactNode } from "react";
 import { UserContext } from "./UserContext";
 
 interface UserType {
@@ -17,12 +17,33 @@ interface UserType {
     __v: number;
 }
 
-export default function({children}:any){
-    const [user, setUser] = useState<UserType>({_id: '',name: '',username: '',email: '',password: '',imageUrl: '',posts: [],savedPosts: [],likedPosts: [],followers: [],following: [],__v: 0})
-
-    return(
-        <UserContext.Provider value={{user , setUser}}>
-           {children}
-        </UserContext.Provider>
-    )
+interface Props {
+    children: ReactNode;
 }
+
+const UserProvider = ({ children }: Props) => {
+    const [user, setUser] = useState<UserType>({
+        _id: '',
+        name: '',
+        username: '',
+        email: '',
+        password: '',
+        imageUrl: '',
+        posts: [],
+        savedPosts: [],
+        likedPosts: [],
+        followers: [],
+        following: [],
+        __v: 0
+    });
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+UserProvider.displayName = "UserProvider";
+
+export default UserProvider;
